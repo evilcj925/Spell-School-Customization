9ffcb36caf258b579a7439de318e3a4f00a97406
evilcj925 <cjkindred925@gmail.com>
2022-02-05 08:49:13 -0800

    basic start

---
+++ scripts/spell-customizatoin.js
@@ -0,0 +1,14 @@
+Hooks.on("ready", () => {
+  CONFIG.DND5E.spellSchools["psm"] = "Psychometabolism";
+  CONFIG.DND5E.spellSchools["mtc"] = "Metacreativity";
+  CONFIG.DND5E.spellSchools["psk"] = "Psychokinesis";
+  CONFIG.DND5E.spellSchools["psp"] = "Psychoportation";
+  CONFIG.DND5E.spellSchools["tlp"] = "Telepathy";
+  CONFIG.DND5E.spellSchools["csc"] = "Clairsentience";
+  CONFIG.DND5E.spellSchools["hmo"] = "Hemomancy";
+  CONFIG.DND5E.weaponTypes["exoticM"] = "Exotic Melee";
+  CONFIG.DND5E.weaponTypes["exoticR"] = "Exotic Ranged";
+  CONFIG.DND5E.weaponProperties["ele"] = "Elegant";
+  CONFIG.DND5E.weaponProperties["mas"] = "Massive";
+  CONFIG.DND5E.weaponProperties["shv"] = "Superheavy";
+});
